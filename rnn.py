54cfc71f23c65e544bb1d54b2ae2ea49627d5007
Michael <micseam@yahoo.com>
2017-09-24 19:58:20 -0700

    Updated runtime mechanics

--- rnn.py
+++ rnn.py
@@ -1,111 +1,14 @@
-'''
-Copied from keras tutorials' char-rnn
-
-
-Example script to generate text from Nietzsche's writings.
-At least 20 epochs are required before the generated text
-starts sounding coherent.
-It is recommended to run this script on GPU, as recurrent
-networks are quite computationally intensive.
-If you try this script on new data, make sure your corpus
-has at least ~100k characters. ~1M is better.
-'''
-
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout
 from keras.layers import LSTM
-from keras.optimizers import Adam
-from keras.callbacks import ModelCheckpoint, EarlyStopping, History
-from callback import Generate_Text
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import scale
 from keras.utils import np_utils
-
 import numpy as np
-import random
-import sys
-import os
-import getopt
-import time
 
-TRAIN_FILE = 'quotes/author-quote.txt'
-TRAIN_FILE = '/home/ubuntu/Datasets/quote.txt'
-WEIGHTS_FILE = ''
-NUM_EPOCHS = 30
-QUICK_MODE = False
-LEARNING_RATE = .001
-DECAY = .5
-LAYER_SIZE = 512
-NUM_LAYERS = 3
-DROPOUT = 0.0
-SEQ_LEN = 40
-
-# Dealing with runtime options
-def main(argv):
-    global TRAIN_FILE
-    global WEIGHTS_FILE
-    global NUM_EPOCHS
-    global QUICK_MODE
-    global DECAY
-    global LEARNING_RATE
-    global LAYER_SIZE
-    global NUM_LAYERS
-    global DROPOUT
-    global SEQ_LEN
-    try:
-        opts, args = getopt.getopt(argv, 't:e:qw:', ['trainFile=','epochs=','quickmode' ,'weightsFile=','learningRate=','decay=', 'numLayers=', 'layerSize=', 'sequenceLength='])
-        for opt, arg in opts:
-            if opt in ('-t', '--trainFile'):
-                TRAIN_FILE = arg
-            elif opt in ('-w', '--weightsFile'):
-                WEIGHTS_FILE = arg
-            elif opt in ('-e', '--epochs'):
-                NUM_EPOCHS = int(arg)
-            elif opt in ('-q', '--quickmode'):
-                QUICK_MODE = True
-            elif opt == '--learningRate':
-                LEARNING_RATE = float(arg)
-            elif opt == '--decay':
-                DECAY = float(arg)
-            elif opt == '--layerSize':
-                LAYER_SIZE = int(arg)
-            elif opt == '--numLayers':
-                NUM_LAYERS = int(arg)
-            elif opt == '--dropout':
-                DROPOUT = float(arg)
-            elif opt == '--sequenceLength':
-                SEQ_LEN = int(arg)
-    except getopt.GetoptError as e:
-        print("No train/weights file provided")
-        print(e)
-
-    print("Using Training File: ", TRAIN_FILE)
-    assert(os.path.exists(TRAIN_FILE))
-
-    if(WEIGHTS_FILE):
-        print("Continuing from previous training.")
-        print("Using Weights File: ", WEIGHTS_FILE)
-        assert(os.path.exists(WEIGHTS_FILE))
-
-    print("Training epochs: ", NUM_EPOCHS)
-    print("Learning Rate: ", LEARNING_RATE)
-    print("Decay: ", DECAY)
-    print("Size of Layers: ", LAYER_SIZE)
-    print("Depth of Network: ", NUM_LAYERS)
-    print("Using dropout: ", DROPOUT)
-
-    if(QUICK_MODE):
-        print("Quick mode enabled.")
-
-    # Preprocessing
-
-    text = open(TRAIN_FILE).read().lower()
+def preprocess_text(filename, SEQ_LEN):
+    text = open(filename).read().lower()
     n_chars = len(text)
-    print('Corpus length: ', n_chars)
-
     chars = sorted(list(set(text)))
     vocab = len(chars)
-    print('Total unique chars: ', vocab)
 
     char_to_idx = dict( (c, i) for i,c in enumerate(chars) )
     idx_to_char = dict( (i, c) for i,c in enumerate(chars) )
@@ -114,31 +17,27 @@ def main(argv):
     sentences = []
     next_chars = []
     for i in range(0, n_chars - SEQ_LEN, step):
-        sentences.append(text[i:i+SEQ_LEN])
+        sentences.append("".join(text[i:i+SEQ_LEN]))
         next_chars.append(text[i+SEQ_LEN])
 
-    n_data = len(sentences)
-    print("Number of sequences: ", n_data)
-
     print("Vectorizing")
+    X, y = vectorize_data(SEQ_LEN, vocab, sentences, char_to_idx, next_chars)
+    return X, y, char_to_idx, idx_to_char, vocab, text
 
-    X = np.zeros( (n_data, SEQ_LEN, vocab), dtype=np.float)
-    y = np.zeros( (n_data, vocab), dtype=np.float)
+def vectorize_data(SEQ_LEN, vocab, sentences, char_to_idx, next_chars):
+    n_data = len(sentences)
+    vectorized_char_to_idx = np.vectorize(char_to_idx.get)
+    sentences_np = np.asarray([list(sentence) for sentence in sentences])
+    X = vectorized_char_to_idx(sentences_np)
+    X = np_utils.to_categorical(X, num_classes=vocab)
+    X = X.reshape( (n_data, SEQ_LEN, vocab) )
 
-    for i, sentence in enumerate(sentences):
-        for t, char in enumerate(sentence):
-            X[i,t, char_to_idx[char]] = 1
-        y[i, char_to_idx[ next_chars[i] ]] = 1
+    y = np_utils.to_categorical(vectorized_char_to_idx(
+        np.asarray(next_chars)), num_classes=vocab)
 
-    if(QUICK_MODE):
-        X = X[:1000]
-        y = y[:1000]
+    return X, y
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-
-
-    # Building a simple LSTM
-    print("Building model")
+def build_model(NUM_LAYERS, SEQ_LEN, vocab, LAYER_SIZE, DROPOUT):
     model = Sequential()
     if(NUM_LAYERS == 1):
         model.add( LSTM(LAYER_SIZE, input_shape=(SEQ_LEN, vocab), return_sequences=False) )
@@ -150,37 +49,4 @@ def main(argv):
         model.add(Dropout(DROPOUT))
     model.add(Dense(vocab))
     model.add(Activation('softmax'))
-    optimizer = Adam(lr=LEARNING_RATE, decay=DECAY, clipvalue=0.5)
-    if(WEIGHTS_FILE):
-        model.load_weights(WEIGHTS_FILE)
-    model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
-    model.summary()
-
-    print("Creating callbacks")
-
-    filepath = "weights/weights-improvement-{epoch:03d}-{loss:.4f}.hdf5"
-    checkpoint = ModelCheckpoint(filepath, save_best_only=True, verbose=1)
-    generate_text = Generate_Text(gen_text_len=100, idx_to_char=idx_to_char, char_to_idx=char_to_idx,text=text, maxlen=SEQ_LEN, vocab=vocab)
-    esCallback = EarlyStopping(min_delta=0, patience=10, verbose=1)
-    hisCallback = History()
-    if(QUICK_MODE):
-        callbacks_list = [checkpoint, hisCallback]
-    else:
-        callbacks_list = [checkpoint, generate_text, hisCallback]
-
-    if not os.path.exists('weights'):
-        os.makedirs('weights')
-
-    print("Saving model hierarchy.")
-    model_json = model.to_json()
-    with open('model.json', 'w') as json_file:
-        json_file.write(model_json)
-
-    print("Training model")
-    hist = model.fit(X_train, y_train, validation_data=(X_test,y_test), batch_size=128, epochs=NUM_EPOCHS, callbacks=callbacks_list)
-    timestr = time.strftime("%m%d-%H%M")
-    np.save('model_history_' + timestr + '.npy', hist.history)
-
-
-if __name__ == "__main__":
-    main(sys.argv[1:])
+    return model
---
+++ train_rnn.py
@@ -0,0 +1,159 @@
+#!/usr/bin/python
+
+from keras.models import load_model
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint, EarlyStopping, History
+from callback import Generate_Text
+from sklearn.model_selection import train_test_split
+from rnn import preprocess_text, build_model
+
+import numpy as np
+import sys
+import os
+import getopt
+import time
+
+TRAIN_FILE = 'quotes/author-quote.txt'
+MODEL_FILE = ''
+WEIGHTS_FILE = ''
+HISTORY_FILE = ''
+NUM_EPOCHS = 30
+QUICK_MODE = False
+LEARNING_RATE = .001
+DECAY = .5
+LAYER_SIZE = 256
+NUM_LAYERS = 2
+DROPOUT = 0.2
+SEQ_LEN = 40
+
+# Dealing with runtime options
+def main(argv):
+    global TRAIN_FILE
+    global MODEL_FILE
+    global WEIGHTS_FILE
+    global NUM_EPOCHS
+    global QUICK_MODE
+    global DECAY
+    global LEARNING_RATE
+    global LAYER_SIZE
+    global NUM_LAYERS
+    global DROPOUT
+    global SEQ_LEN
+    try:
+        opts, args = getopt.getopt(argv, 't:e:qm:w:h:', ['trainFile=','epochs=',
+            'quickmode' ,'modelFile=', 'weightsFile=','learningRate=','decay=',
+            'numLayers=', 'layerSize=', 'sequenceLength=', 'historyFile='])
+        for opt, arg in opts:
+            if opt in ('-t', '--trainFile'):
+                TRAIN_FILE = arg
+            elif opt in ('-m', '--modelFile'):
+                MODEL_FILE = arg
+            elif opt in ('-w', '--weightsFile'):
+                WEIGHTS_FILE = arg
+            elif opt in ('-h', '--historyFile'):
+                HISTORY_FILE = arg
+            elif opt in ('-e', '--epochs'):
+                NUM_EPOCHS = int(arg)
+            elif opt in ('-q', '--quickmode'):
+                QUICK_MODE = True
+            elif opt == '--learningRate':
+                LEARNING_RATE = float(arg)
+            elif opt == '--decay':
+                DECAY = float(arg)
+            elif opt == '--layerSize':
+                LAYER_SIZE = int(arg)
+            elif opt == '--numLayers':
+                NUM_LAYERS = int(arg)
+            elif opt == '--dropout':
+                DROPOUT = float(arg)
+            elif opt == '--sequenceLength':
+                SEQ_LEN = int(arg)
+    except getopt.GetoptError as e:
+        print(e)
+
+    print("Using Training File: ", TRAIN_FILE)
+    print("Preprocessing...")
+    X, y, char_to_idx, idx_to_char, vocab, text = preprocess_text(
+        filename=TRAIN_FILE, SEQ_LEN=SEQ_LEN)
+
+    if(QUICK_MODE):
+        X = X[:1000]
+        y = y[:1000]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+    # Building a simple LSTM
+    if(MODEL_FILE):
+        print("Loading model")
+        model = load_model(MODEL_FILE)
+    else:
+        print("Building model")
+        print("Size of Layers: ", LAYER_SIZE)
+        print("Depth of Network: ", NUM_LAYERS)
+        print("Using dropout: ", DROPOUT)
+
+        model = build_model(NUM_LAYERS=NUM_LAYERS, SEQ_LEN=SEQ_LEN, vocab=vocab,
+            LAYER_SIZE=LAYER_SIZE, DROPOUT=DROPOUT)
+
+    if(WEIGHTS_FILE):
+        model.load_weights(WEIGHTS_FILE)
+    optimizer = Adam(lr=LEARNING_RATE, decay=DECAY, clipvalue=0.5)
+    model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+
+    model.summary()
+
+    if(HISTORY_FILE):
+        history = np.load(HISTORY_FILE).item()
+    else:
+        history = {}
+
+
+    # Setting up models directory
+    if not os.path.exists('models'):
+        os.makedirs('models')
+
+    timestr = time.strftime("%m%d-%H%M")
+    model_name = "CharRNN_" + timestr
+    model_dir = os.path.join('models', model_name)
+
+    if not os.path.exists(model_dir):
+        os.makedirs(model_dir)
+        print("Saving model data to:", os.path.abspath(model_dir))
+        model.save(os.path.join(model_dir, "model.h5"))
+
+    print("Creating callbacks")
+
+
+
+    weights_filepath = os.path.join(model_dir,
+        "weights-improvement-{epoch:03d}-{loss:.4f}.hdf5")
+    checkpoint = ModelCheckpoint(weights_filepath, save_best_only=True,
+        verbose=1)
+    generate_text = Generate_Text(gen_text_len=300, idx_to_char=idx_to_char,
+        char_to_idx=char_to_idx, text=text, maxlen=SEQ_LEN, vocab=vocab)
+    esCallback = EarlyStopping(min_delta=0, patience=10, verbose=1)
+    hisCallback = History()
+    if(QUICK_MODE):
+        callbacks_list = [checkpoint, hisCallback]
+    else:
+        callbacks_list = [checkpoint, generate_text, hisCallback]
+
+
+    print("Training model")
+    for e in range(NUM_EPOCHS):
+        try:
+            print("\nEPOCH {}\n".format(e))
+            hist = model.fit(X_train, y_train, validation_data=(X_test,y_test),
+                batch_size=128, epochs=1, callbacks=callbacks_list)
+            for k, v in hist.history.items():
+                history[k] = history[k] + v
+        except KeyboardInterrupt:
+            print("Exiting training loop")
+            break
+    print("Saving training history")
+    history_filename = os.path.join(model_dir, 'model_history.npy')
+    np.save(history_filename, history)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
